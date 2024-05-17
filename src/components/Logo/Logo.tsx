9dfda12be3fc45250b55f177f23bb8da6093e686
Chris Jardine <chris@keplux.com>
2024-05-16 23:51:01 -0400

    Add link to home.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,4 +1,5 @@
 import { Image, Stack, StackProps, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 export default function Logo({
   containerProps,
@@ -6,14 +7,29 @@ export default function Logo({
   containerProps?: StackProps;
 }) {
   return (
-    <Stack spacing={0} alignItems="center" {...containerProps}>
+    <Stack
+      as={NextLink}
+      href="/"
+      spacing={0}
+      alignItems="center"
+      role="group"
+      {...containerProps}
+    >
       <Image
         src="/images/creative-living-services-logo.jpg"
         alt="Creative Living Services logo of people holding hands, including a person in a wheelchair."
         h="auto"
         w={32}
       />
-      <Text fontSize="sm">Creative Living Services</Text>
+      <Text
+        fontSize="sm"
+        fontWeight="bold"
+        _groupHover={{
+          color: "blue.700",
+        }}
+      >
+        Creative Living Services
+      </Text>
     </Stack>
   );
 }
