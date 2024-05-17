54067974142447c54e16ebac0d0cff6eea14f12b
Chris Jardine <chris@keplux.com>
2024-05-17 16:10:23 -0400

    Update styles.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,7 @@ import {
   Avatar,
   Container,
   HStack,
+  Image,
   ListItem,
   SimpleGrid,
   Stack,
@@ -15,72 +16,114 @@ export default function AboutPage() {
   return (
     <>
       <Head>
-        <title>About Creative Living Services Services</title>
+        <title>About Creative Living Services</title>
         <meta
           name="description"
-          content="Providing homemaker care, transportation, vocational habilitation, and adult day services for individuals with cognitive or developmental disabilities."
+          content="Creative Living Services is dedicated to improving lives through personalized care, committed staff, and community partnerships. Discover our mission and values."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container as={Stack} spacing={8} maxW="container.md">
-        <Stack>
-          <Heading as="h1">Our Mission</Heading>
-          <Text>
-            The mission of Creative Living Services is to provide all
-            individuals with the highest quality care available while
-            maintaining individual dignity. We demonstrate this commitment by:
+      <Stack spacing={16}>
+        <Container as={Stack} maxW="container.md">
+          <Heading as="h1">
+            Creative Living Services is dedicated to enhancing quality of life
+          </Heading>
+          <Text color="gray.600">
+            We are a small but active organization dedicated to improving the
+            quality of life for the individuals we serve. Our team diligently
+            executes individualized support plans tailored to each person&apos;s
+            needs and aspirations.
           </Text>
-          <UnorderedList>
-            <ListItem>
-              Operating within a people centered philosophy. We believe shared
-              humanity dictates the basis of the relationship between service
-              provider and client, and that the client should take a central
-              role in determining the care they receive;
-            </ListItem>
-            <ListItem>
-              Employing committed staff, providing them with all necessary
-              training to deliver the promises of Creative Living Services;
-            </ListItem>
-            <ListItem>
-              Promoting a work environment that embraces both similarities and
-              differences, respects individuality, and promotes personal and
-              professional growth;
-            </ListItem>
-            <ListItem>
-              Developing relationships with community partners, health care
-              professionals, and other resources to enhance the life experiences
-              of our clients; and
-            </ListItem>
-            <ListItem>
-              Demonstrating social responsibility by giving back to our
-              community.
-            </ListItem>
-          </UnorderedList>
-          <Heading as="h2">Our Team</Heading>
-          <SimpleGrid columns={{ base: 1, sm: 2 }}>
-            <HStack>
-              <Avatar src="/images/amy-thompson.jpg" size='lg' />
-              <Stack spacing={0}>
-                <Text fontWeight="bold">Amy Thompson</Text>
-                <Text fontSize="sm" color="gray.600">
-                  CEO/Director
-                </Text>
-              </Stack>
-            </HStack>
-            <HStack>
-              <Avatar src="/images/ashley-taylor.jpg" size='lg' />
-              <Stack spacing={0}>
-                <Text fontWeight="bold">Ashley Taylor</Text>
-                <Text fontSize="sm" color="gray.600">
-                  Assistant Director
-                </Text>
-              </Stack>
-            </HStack>
-          </SimpleGrid>
-        </Stack>
-      </Container>
+        </Container>
+
+        <SimpleGrid
+          as={Container}
+          maxW="container.xl"
+          columns={{ base: 1, lg: 5 }}
+          gap={{ base: 8, sm: 16 }}
+        >
+          <Stack gridColumn={{ lg: "1 / span 3" }}>
+            <Heading as="h2">Our Mission</Heading>
+            <Text>
+              The mission of Creative Living Services is to provide all
+              individuals with the highest quality care available while
+              maintaining individual dignity. We demonstrate this commitment by:
+            </Text>
+            <UnorderedList spacing={4}>
+              <ListItem>
+                Operating within a people centered philosophy. We believe shared
+                humanity dictates the basis of the relationship between service
+                provider and client, and that the client should take a central
+                role in determining the care they receive;
+              </ListItem>
+              <ListItem>
+                Employing committed staff, providing them with all necessary
+                training to deliver the promises of Creative Living Services;
+              </ListItem>
+              <ListItem>
+                Promoting a work environment that embraces both similarities and
+                differences, respects individuality, and promotes personal and
+                professional growth;
+              </ListItem>
+              <ListItem>
+                Developing relationships with community partners, health care
+                professionals, and other resources to enhance the life
+                experiences of our clients; and
+              </ListItem>
+              <ListItem>
+                Demonstrating social responsibility by giving back to our
+                community.
+              </ListItem>
+            </UnorderedList>
+          </Stack>
+
+          <Stack
+            position="relative"
+            gridColumn={{ lg: "4 / span 2" }}
+            p={{ base: 4, sm: 8 }}
+            rounded="2xl"
+            bg="white"
+            h="fit-content"
+            overflow="hidden"
+          >
+            <Image
+            display={{base: 'none', md: 'block'}}
+              src="/graphics/squares.png"
+              alt="Squares graphic"
+              position="absolute"
+              top={10}
+              right={-12}
+              w="50%"
+              transform="rotate(-90deg)"
+            />
+            <Stack position="relative">
+              <Heading as="h2">Our Team</Heading>
+              <SimpleGrid columns={{base: 1, sm: 2, lg: 1}} spacing={8}>
+                <HStack>
+                  <Avatar src="/images/amy-thompson.jpg" size="lg" />
+                  <Stack spacing={0}>
+                    <Text fontWeight="bold">Amy Thompson</Text>
+                    <Text fontSize="sm" color="gray.600">
+                      CEO/Director
+                    </Text>
+                  </Stack>
+                </HStack>
+                <HStack>
+                  <Avatar src="/images/ashley-taylor.jpg" size="lg" />
+                  <Stack spacing={0}>
+                    <Text fontWeight="bold">Ashley Taylor</Text>
+                    <Text fontSize="sm" color="gray.600">
+                      Assistant Director
+                    </Text>
+                  </Stack>
+                </HStack>
+              </SimpleGrid>
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Stack>
     </>
   );
 }
