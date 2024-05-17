f9254b1d338801b9201ba556c180c95e78c52a0a
Chris Jardine <chris@keplux.com>
2024-05-17 16:10:35 -0400

    Redesign layout.

--- src/pages/ads-vh.tsx
+++ src/pages/ads-vh.tsx
@@ -1,6 +1,19 @@
 import { Heading } from "@/components/Heading";
-import { Container, Stack, Text } from "@chakra-ui/react";
+import { SplitImageBlock } from "@/components/SplitImageBlock";
+import {
+  Container,
+  HStack,
+  Icon,
+  Link,
+  Square,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import NextLink from "next/link";
+import { FaChevronRight } from "react-icons/fa";
+import { PiBroomFill } from "react-icons/pi";
+import { RiCandleFill, RiFileShredFill } from "react-icons/ri";
 
 export default function AdsVhPage() {
   return (
@@ -9,51 +22,135 @@ export default function AdsVhPage() {
         <title>Adult Day Services & Vocational Habilitation</title>
         <meta
           name="description"
-          content="Providing homemaker care, transportation, vocational habilitation, and adult day services for individuals with cognitive or developmental disabilities."
+          content="Discover our Adult Day Services and Vocational Habilitation programs offering paid employment and activities for adults with developmental disabilities."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container as={Stack} spacing={8} maxW="container.md">
-        <Stack>
+      <Stack spacing={16}>
+        <Container as={Stack} maxW="container.md">
           <Heading as="h1">
-            Adult Day Services & Vocational Habilitation
+            Empowering Adults with Developmental Disabilities through Adult Day
+            Services and Vocational Habilitation
           </Heading>
-          <Text>
-            Our Adult Day Services provides consumers with an opportunity to
-            socialize, learn, and participate in activities that enhance their
-            daily life. Vocational Habilitation provides these individuals with
-            paid employment for services that give back to the community.
-            Here&apos;s a little more about us...
+          <Text color="gray.600">
+            Our Adult Day Services provide a nurturing environment where adults
+            with developmental disabilities can socialize, learn, and engage in
+            activities that enhance their daily lives. Through our Vocational
+            Habilitation program, participants gain meaningful, paid employment
+            opportunities that benefit both themselves and the community.
           </Text>
-          <Text>
-            We are a small, but active, company that provides services to adults
-            with developmental disabilities. Our team executes individualized
-            support plans to improve the quality of life for those we serve.
-          </Text>
-          <Text>
-            Our Adult Day Services and Vocational Habilitation programs provide
-            paid employment and recreational activities for many of our
-            consumers. The work they do contributes to the betterment of our
-            community! Currently, our consumers make candles, perform document
-            destruction, and offer cleaning services.
-          </Text>
-          <Text>
-            Profit from these services pay the individuals performing the tasks,
-            and help fund the recreational activities our Adult Day Services
-            program offers. To make this all possible, we need support from you!
-            With your candle order today, you&apos;re making a difference!
-          </Text>
-          <Text>
-            We invite you to take advantage of our competitive rates for
-            document destruction and cleaning as well! These services are
-            supervised by trained staff, and we will make sure you&apos;re
-            satisfied by the quality of our work.
-          </Text>
-          <Text>Please call or e-mail for more information!</Text>
-        </Stack>
-      </Container>
+        </Container>
+
+        <Container
+          as={Stack}
+          position="relative"
+          spacing={16}
+          maxW="container.xl"
+        >
+          <SplitImageBlock imgSrc="/images/candle-making.jpg">
+            <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
+              Vocational Habilitation
+            </Heading>
+
+            <Stack>
+              <Text>
+                Our programs not only offer recreational activities but also
+                involve our consumers in productive work that contributes to the
+                community. Currently, our participants are engaged in:
+              </Text>
+              <HStack mt={4} alignItems="flex-start">
+                <Square bg="blue.600" size={16} rounded="2xl">
+                  <Icon as={RiCandleFill} color="white" boxSize={5} />
+                </Square>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    Candle-making
+                  </Text>
+                  <Text fontSize="sm" color="gray.600">
+                    Crafting beautiful candles available for purchase.
+                  </Text>
+                </Stack>
+              </HStack>
+              <HStack alignItems="flex-start">
+                <Square bg="blue.600" size={16} rounded="2xl">
+                  <Icon as={RiFileShredFill} color="white" boxSize={5} />
+                </Square>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    Document destruction
+                  </Text>
+                  <Text fontSize="sm" color="gray.600">
+                    Providing secure and reliable document shredding services.
+                  </Text>
+                </Stack>
+              </HStack>
+              <HStack alignItems="flex-start">
+                <Square bg="blue.600" size={16} rounded="2xl">
+                  <Icon as={PiBroomFill} color="white" boxSize={5} />
+                </Square>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight="bold">
+                    Cleaning
+                  </Text>
+                  <Text fontSize="sm" color="gray.600">
+                    Offering professional cleaning services for homes and
+                    businesses.
+                  </Text>
+                </Stack>
+              </HStack>
+            </Stack>
+          </SplitImageBlock>
+
+          <SplitImageBlock
+            imgSrc="/images/paying.jpg"
+            flip
+            imgProps={{
+              bgPosition: "50% 30%",
+              bgSize: "cover",
+            }}
+          >
+            <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
+              How You Can Support Us
+            </Heading>
+            <Text>
+              The revenue generated from these activities directly supports the
+              individuals performing the tasks and funds the recreational
+              activities in our Adult Day Services program. Your support is
+              crucial in sustaining these initiatives. By purchasing our
+              candles, or utilizing our document destruction and cleaning
+              services, you are making a significant impact on the lives of our
+              participants.
+            </Text>
+          </SplitImageBlock>
+
+          <Container as={Stack} maxW="container.md">
+            <Heading as="h3">
+              Want to make an impact?
+            </Heading>
+            <Text color="gray.600">
+              We invite you to experience our competitive rates and exceptional
+              service quality, all supervised by our trained staff to ensure
+              your complete satisfaction.
+            </Text>
+            <Link
+              as={NextLink}
+              href="/contact"
+              variant="navLinkOutline"
+              mt={8}
+              display="flex"
+              alignItems="center"
+              gap={2}
+              w="fit-content"
+              alignSelf="center"
+            >
+              Contact us
+              <Icon as={FaChevronRight} />
+            </Link>
+          </Container>
+        </Container>
+      </Stack>
     </>
   );
 }
