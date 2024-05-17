fa5eb35644d5ef0a568359d17602f964c635f18d
Chris Jardine <chris@keplux.com>
2024-05-17 02:54:20 -0400

    Adjust inputs.

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,26 +1,59 @@
+import { phoneMaskOptions } from "@/constants";
 import {
   Button,
+  Flex,
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   Stack,
   Textarea,
 } from "@chakra-ui/react";
+import { useMaskito } from "@maskito/react";
+import { US } from "country-flag-icons/react/3x2";
 
 export default function ContactForm() {
+  const phoneMaskRef = useMaskito({ options: phoneMaskOptions });
+
+  function Flag() {
+    return (
+      <Flex w={6} h={6} rounded="full" overflow="hidden" shadow="md">
+        <US
+          title="United States"
+          style={{
+            transform: "scale(1.75)",
+          }}
+        />
+      </Flex>
+    );
+  }
+
   return (
-    <Stack as='form' spacing={4}>
+    <Stack as="form" spacing={4}>
       <FormControl>
         <FormLabel>Name</FormLabel>
         <Input />
       </FormControl>
       <FormControl>
         <FormLabel>Email</FormLabel>
-        <Input />
+        <Input inputMode="email" autoCapitalize="none" />
       </FormControl>
       <FormControl>
         <FormLabel>Phone number</FormLabel>
-        <Input />
+        <InputGroup>
+          <Input
+            ref={phoneMaskRef}
+            type="tel"
+            inputMode="tel"
+            // onInput={(e) => setValue("phone", e.currentTarget.value)}
+            // onBlur={onBlur}
+            // value={value}
+          />
+          <InputRightElement pointerEvents="none">
+            <Flag />
+          </InputRightElement>
+        </InputGroup>
       </FormControl>
       <FormControl>
         <FormLabel>Message</FormLabel>
