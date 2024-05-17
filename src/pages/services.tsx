d3daeae1701f50c2746cb0b7e54931cbb1b0f725
Chris Jardine <chris@keplux.com>
2024-05-16 23:41:18 -0400

    Update services page.

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,6 +1,7 @@
 import { Heading } from "@/components/Heading";
 import { SplitImageBlock } from "@/components/SplitImageBlock";
 import {
+  Box,
   Container,
   Icon,
   Link,
@@ -41,12 +42,17 @@ export default function ServicesPage() {
           </Text>
         </Container>
 
-        <Container as={Stack} spacing={16} maxW="container.xl">
+        <Container
+          as={Stack}
+          position="relative"
+          spacing={16}
+          maxW="container.xl"
+        >
           <SplitImageBlock imgSrc="/images/home-care.jpg">
             <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
               Homemaker Personal Care Services
             </Heading>
-            <Text color="blue.700">
+            <Text color="blue.800">
               Our Homemaker Personal Care Services include assisting individuals
               with activities of daily living such as personal hygiene,
               dressing, feeding, and mobility needs. We focus on skills
@@ -59,7 +65,7 @@ export default function ServicesPage() {
             <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
               Medical and Non-Medical Transportation
             </Heading>
-            <Text color="blue.700">
+            <Text color="blue.800">
               We offer transportation services to ensure waiver enrollees can
               access essential programs such as adult day support, vocational
               habilitation, supported employment enclave, and community
@@ -69,11 +75,15 @@ export default function ServicesPage() {
             </Text>
           </SplitImageBlock>
 
-          <SplitImageBlock imgSrc="/images/day-services.jpg">
+          <SplitImageBlock
+            imgSrc="/images/day-services.jpg"
+            contentProps={{ shadow: "lg" }}
+            imgProps={{ shadow: "lg" }}
+          >
             <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
               Adult Day Services
             </Heading>
-            <Text color="blue.700">
+            <Text color="blue.800">
               Our Adult Day Services are provided separately from any home or
               residential facility, focusing on non-work activities that enhance
               personal development and social interaction. We strive to create
@@ -86,114 +96,246 @@ export default function ServicesPage() {
           </SplitImageBlock>
         </Container>
 
-        <SimpleGrid
-          columns={{ base: 1, md: 2, xl: 5 }}
-          borderWidth={2}
+        <Box
+          borderTopWidth={2}
+          borderBottomWidth={2}
           borderColor="blue.200"
-          mt={8}
-          py={16}
+          mt={-32}
+          pt={32}
+          pb={16}
           px={4}
-          bg="white"
-          spacing={8}
+          bg="blue.600"
           shadow="lg"
         >
-          <Stack gridColumn={{ md: "1", xl: "1" }}>
-            <Heading as="h4" fontSize="lg">
-              Assessment
-            </Heading>
-            <Text fontSize="sm" color="gray.700">
-              Assessment can be formal or informal and is conducted for the
-              purpose of developing an Individual Service Plan (ISP).
-            </Text>
-          </Stack>
-          <Stack gridColumn={{ md: "2", xl: "2" }}>
-            <Heading as="h4" fontSize="lg">
-              Personal Care
-            </Heading>
-            <Text fontSize="sm" color="gray.700">
-              Personal care services include assistance with personal hygiene,
-              eating, communication, mobility, toileting, and dressing.
-            </Text>
-          </Stack>
-          <Stack gridColumn={{ md: "1 / span 2", xl: "3" }}>
-            <Heading as="h4" fontSize="lg">
-              Skill Reinforcement
-            </Heading>
-            <Text fontSize="sm" color="gray.700">
-              Skill reinforcement involves implementing behavioral intervention
-              plans and providing help with the use of communication and
-              mobility devices.
-            </Text>
-          </Stack>
-          <Stack gridColumn={{ md: "1 / span 2", xl: "4" }}>
-            <Heading as="h4" fontSize="lg">
-              Training in Self-Determination
-            </Heading>
-            <Text fontSize="sm" color="gray.700">
-              Training in self-determination includes developing self-advocacy
-              skills and acquiring abilities that enable individuals to become
-              more independent.
-            </Text>
-          </Stack>
-          <Stack gridColumn={{ md: "1 / span 2", xl: "5" }}>
-            <Heading as="h4" fontSize="lg">
-              Recreation and Leisure
-            </Heading>
-            <Text fontSize="sm" color="gray.700">
-              Recreation and leisure activities include therapeutic supports
-              identified in the ISP that help to develop and maintain social
-              relationships and family contacts.
-            </Text>
-          </Stack>
+          <Container maxW="1920px">
+            <SimpleGrid columns={{ base: 1, md: 2, xl: 5 }} spacing={8}>
+              <Stack gridColumn={{ md: "1", xl: "1" }}>
+                <Heading as="h4" fontSize="lg" color="blue.50">
+                  Assessment
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Assessment can be formal or informal and is conducted for the
+                  purpose of developing an Individual Service Plan (ISP).
+                </Text>
+              </Stack>
+              <Stack gridColumn={{ md: "2", xl: "2" }}>
+                <Heading as="h4" fontSize="lg" color="blue.50">
+                  Personal Care
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Personal care services include assistance with personal
+                  hygiene, eating, communication, mobility, toileting, and
+                  dressing.
+                </Text>
+              </Stack>
+              <Stack gridColumn={{ md: "1 / span 2", xl: "3" }}>
+                <Heading as="h4" fontSize="lg" color="blue.50">
+                  Skill Reinforcement
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Skill reinforcement involves implementing behavioral
+                  intervention plans and providing help with the use of
+                  communication and mobility devices.
+                </Text>
+              </Stack>
+              <Stack gridColumn={{ md: "1 / span 2", xl: "4" }}>
+                <Heading as="h4" fontSize="lg" color="blue.50">
+                  Training in Self-Determination
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Training in self-determination includes developing
+                  self-advocacy skills and acquiring abilities that enable
+                  individuals to become more independent.
+                </Text>
+              </Stack>
+              <Stack gridColumn={{ md: "1 / span 2", xl: "5" }}>
+                <Heading as="h4" fontSize="lg" color="blue.50">
+                  Recreation and Leisure
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Recreation and leisure activities include therapeutic supports
+                  identified in the ISP that help to develop and maintain social
+                  relationships and family contacts.
+                </Text>
+              </Stack>
 
-          <Stack
-            gridColumn={{ md: "1 / span 2", xl: "1 / span 5" }}
-            alignItems="center"
-          >
-            <Link
-              as={NextLink}
-              href="/ads-vh"
-              variant="navLinkOutline"
-              display="flex"
-              alignItems="center"
-              gap={2}
+              <Stack
+                gridColumn={{ md: "1 / span 2", xl: "1 / span 5" }}
+                alignItems="center"
+              >
+                <Link
+                  as={NextLink}
+                  href="/ads-vh"
+                  variant="outline"
+                  display="flex"
+                  alignItems="center"
+                  gap={2}
+                >
+                  Adult day services <Icon as={FaChevronRight} />
+                </Link>
+              </Stack>
+            </SimpleGrid>
+          </Container>
+        </Box>
+
+        <Container as={Stack} maxW="container.md"></Container>
+
+        <Box>
+          <Container maxW="container.lg">
+            <SimpleGrid
+              columns={{ base: 1, lg: 2 }}
+              gap={{ base: 8, sm: 16 }}
+              position="relative"
             >
-              Adult day services <Icon as={FaChevronRight} />
-            </Link>
-          </Stack>
-        </SimpleGrid>
+              <Box
+                rounded="2xl"
+                bgImg="url('/images/supported-employment.jpg')"
+                bgSize="cover"
+                bgPosition="right"
+                minH={{ base: 72, md: "39rem" }}
+              />
+              <Stack>
+                <Heading as="h1">Supported employment services</Heading>
+                <Text color="gray.700">
+                  Supported employment services offer intensive, ongoing support
+                  to help individuals perform work in regular employment
+                  settings, including self-employment. These services are
+                  designed to ensure sustained success and integration in the
+                  workplace, providing tailored assistance that addresses each
+                  individual&apos;s unique strengths and challenges. By offering
+                  comprehensive support, we help individuals develop the skills
+                  and confidence needed to thrive in their chosen employment
+                  environment.
+                </Text>
+              </Stack>
+            </SimpleGrid>
+          </Container>
 
-        <Container as={Stack} spacing={16} maxW="container.xl">
-          <SplitImageBlock imgSrc="/images/supported-employment.jpg" flip>
+          <Container
+            as={Box}
+            mt={{ base: 8, lg: -64 }}
+            py={{ base: 0, lg: 16 }}
+            rounded={{ base: "none", lg: "2xl" }}
+            bg={{ lg: "blue.100" }}
+            maxW="container.xl"
+          >
+            <SimpleGrid columns={{ base: 1, lg: 2 }} gap={{ base: 8, sm: 16 }}>
+              <Stack gridColumn={{ lg: "2" }} spacing={8}>
+                <Stack>
+                  <Heading as="h4" fontSize="lg" color={{ lg: "blue.900" }}>
+                    Enclave
+                  </Heading>
+                  <Text
+                    fontSize="sm"
+                    color={{ base: "gray.700", lg: "blue.800" }}
+                  >
+                    Individuals work as a team at a single work-site (community
+                    business or industry) with continuous on-site support
+                    provided by the staff.
+                  </Text>
+                </Stack>
+
+                <Stack>
+                  <Heading as="h4" fontSize="lg" color={{ lg: "blue.900" }}>
+                    Community
+                  </Heading>
+                  <Text
+                    fontSize="sm"
+                    color={{ base: "gray.700", lg: "blue.800" }}
+                  >
+                    Individuals work in integrated community work settings
+                    alongside employees without disabilities, performing the
+                    same or similar tasks.
+                  </Text>
+                </Stack>
+              </Stack>
+            </SimpleGrid>
+          </Container>
+        </Box>
+
+        {/* <Container
+          as={Stack}
+          position="relative"
+          spacing={16}
+          maxW="container.xl"
+        >
+          <SplitImageBlock
+            imgSrc="/images/supported-employment.jpg"
+            flip
+            contentProps={{
+              shadow: { md: "lg" },
+            }}
+            imgProps={{
+              bgPosition: "right",
+              shadow: { md: "lg" },
+            }}
+          >
             <Heading as="h2" mt={4} fontSize="2xl" color="blue.900">
-              Supported Employment
+              Supported Employment Services
             </Heading>
-            <Text color="blue.700">
+            <Text color="blue.800">
               Supported employment services offer intensive, ongoing support to
               help individuals perform work in regular employment settings,
               including self-employment. These services are designed to ensure
-              sustained success and integration in the workplace.
+              sustained success and integration in the workplace, providing
+              tailored assistance that addresses each individual&apos;s unique
+              strengths and challenges. By offering comprehensive support, we
+              help individuals develop the skills and confidence needed to
+              thrive in their chosen employment environment.
             </Text>
-            <Stack mt={-12}>
-              <Heading as="h3" fontSize="lg" color="blue.900">
-                Enclave
-              </Heading>
-              <Text fontSize="sm" color="blue.700">
-                Individuals work as a team at a single work-site (community
-                business or industry) with continuous on-site support provided
-                by the staff.
-              </Text>
-              <Heading as="h3" fontSize="lg" color="blue.900">
-                Community
-              </Heading>
-              <Text fontSize="sm" color="blue.700">
-                Individuals work in integrated community work settings alongside
-                employees without disabilities, performing the same or similar
-                tasks.
-              </Text>
-            </Stack>
           </SplitImageBlock>
         </Container>
+
+        <Box
+          borderTopWidth={2}
+          borderBottomWidth={2}
+          borderColor="blue.200"
+          mt={-32}
+          pt={32}
+          pb={16}
+          px={4}
+          bg="blue.600"
+          shadow="lg"
+        >
+          <Container maxW="container.md">
+            <SimpleGrid columns={{ md: 2 }} spacing={8}>
+              <Stack>
+                <Heading as="h4" fontSize="lg" color="blue.100">
+                  Enclave
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Individuals work as a team at a single work-site (community
+                  business or industry) with continuous on-site support provided
+                  by the staff.
+                </Text>
+              </Stack>
+
+              <Stack>
+                <Heading as="h4" fontSize="lg" color={"blue.100"}>
+                  Community
+                </Heading>
+                <Text fontSize="sm" color="blue.100">
+                  Individuals work in integrated community work settings
+                  alongside employees without disabilities, performing the same
+                  or similar tasks.
+                </Text>
+              </Stack>
+
+              <Stack alignItems="center" gridColumn={{ md: "1 / span 2" }}>
+                <Link
+                  as={NextLink}
+                  href="/ads-vh"
+                  variant="outline"
+                  display="flex"
+                  alignItems="center"
+                  gap={2}
+                >
+                  Employment services <Icon as={FaChevronRight} />
+                </Link>
+              </Stack>
+            </SimpleGrid>
+          </Container>
+        </Box> */}
       </Stack>
     </>
   );
