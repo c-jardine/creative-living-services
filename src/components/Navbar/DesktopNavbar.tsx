6f4331590dec59720bbde9500f1bb3d24ffe1c0b
Chris Jardine <chris@keplux.com>
2024-05-17 18:26:45 -0400

    Show active nav link indicator.

--- src/components/Navbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar.tsx
@@ -1,8 +1,15 @@
 import { Box, Container, HStack, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 import { Logo } from "../Logo";
 
 export default function DesktopNavbar() {
+  const router = useRouter();
+
+  function getLinkColor(url: string) {
+    return router.asPath === url ? "blue.600" : "gray.600";
+  }
+
   return (
     <Box
       display={{ base: "none", lg: "block" }}
@@ -13,22 +20,54 @@ export default function DesktopNavbar() {
         <HStack justifyContent="space-between">
           <Logo />
           <HStack as="nav">
-            <Link as={NextLink} href="/" variant="navLink">
+            <Link
+              as={NextLink}
+              href="/"
+              variant="navLink"
+              color={getLinkColor("/")}
+            >
               Home
             </Link>
-            <Link as={NextLink} href="/services" variant="navLink">
+            <Link
+              as={NextLink}
+              href="/services"
+              variant="navLink"
+              color={getLinkColor("/services")}
+            >
               Services
             </Link>
-            <Link as={NextLink} href="/ads-vh" variant="navLink">
+            <Link
+              as={NextLink}
+              href="/ads-vh"
+              variant="navLink"
+              color={getLinkColor("/ads-vh")}
+            >
               ADS/VH
             </Link>
-            <Link as={NextLink} href="/about" variant="navLink">
+            <Link
+              as={NextLink}
+              href="/about"
+              variant="navLink"
+              color={getLinkColor("/about")}
+            >
               About
             </Link>
-            <Link as={NextLink} href="/contact" variant="navLink">
+            <Link
+              as={NextLink}
+              href="/contact"
+              variant="navLink"
+              color={getLinkColor("/contact")}
+            >
               Contact
             </Link>
-            <Link as={NextLink} href="/careers" variant="navLinkOutline" ml={4}>
+            <Link
+              as={NextLink}
+              href="/careers"
+              variant="navLinkOutline"
+              bg={router.asPath === "/careers" ? "blue.600" : "white"}
+              color={router.asPath === "/careers" ? "white" : "blue.600"}
+              ml={4}
+            >
               Careers
             </Link>
           </HStack>
