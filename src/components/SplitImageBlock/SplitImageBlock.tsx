ad7ac516e86dc6a64cc6f2a7007ec3eb8062f21f
Chris Jardine <chris@keplux.com>
2024-05-18 00:17:21 -0400

    Add containerProps to props.

--- src/components/SplitImageBlock/SplitImageBlock.tsx
+++ src/components/SplitImageBlock/SplitImageBlock.tsx
@@ -1,4 +1,11 @@
-import { Box, BoxProps, SimpleGrid, Stack, StackProps } from "@chakra-ui/react";
+import {
+  Box,
+  BoxProps,
+  SimpleGrid,
+  SimpleGridProps,
+  Stack,
+  StackProps,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function SplitImageBlock({
@@ -7,15 +14,17 @@ export default function SplitImageBlock({
   flip = false,
   contentProps,
   imgProps,
+  containerProps,
 }: {
   children: React.ReactNode;
   imgSrc: string;
   flip?: boolean;
   contentProps?: StackProps;
   imgProps?: BoxProps;
+  containerProps?: SimpleGridProps;
 }) {
   return (
-    <SimpleGrid columns={{ base: 1, md: 5 }} gap={8}>
+    <SimpleGrid columns={{ base: 1, md: 5 }} gap={8} {...containerProps}>
       <Stack
         gridColumn={{ base: "1", md: flip ? "4 / span 2" : "1 / span 2" }}
         spacing={{ base: 4, md: 16 }}
